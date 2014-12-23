5c3593ca8e42567206e1c223fd3be40ce71a5f3f
wp-buddy <wp-buddy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-23 10:48:49 +0000

    Added version 0.2.1 to tags
    git-svn-id: https://plugins.svn.wordpress.org/rss-itunes-redirection/tags/0.2.1@1052431 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ rss-itunes-redirection.php
@@ -0,0 +1,44 @@
+<?php 
+/*
+Plugin Name: iTunes RSS Redirection
+Plugin URI: http://wp-buddy.com
+Description: A Plugin that redirects the current iTunes feed to another feed url.
+Version: 0.2
+Author: wp-buddy
+Author URI: http://wp-buddy.com
+*/
+/*  Copyright 2012-2014  WP-Buddy (email : info@wp-buddy.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+define('ITRSSR', ABSPATH.'/wp-content/plugins/rss-itunes-redirection');
+
+require_once(ITRSSR.'/rss-itunes-redirection-class.php');
+
+// Crate a new instance
+$iTRSS = new iTunesRSSRedirection();
+
+// Load the translation
+$iTRSS->init_translation();
+
+// Create Menue
+$iTRSS->create_wp_menu();
+
+// Create Settings-Page
+$iTRSS->create_wp_settings();
+
+// add redirection
+$iTRSS->redirection();
